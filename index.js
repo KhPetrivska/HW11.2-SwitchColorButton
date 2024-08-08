99dd822264810dd6ecbe9d730b775ed12ec51c77
Kristina <kristin.petrivska@gmail.com>
2024-08-08 15:14:24 -0600

    add code

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+const textBlock = document.createElement("p");
+textBlock.textContent = `There is a text block on the page and a button.
+When the button is clicked, the text changes color.
+When the button is clicked again, the color returns to the previous one.`;
+document.body.append(textBlock);
+
+const button = document.createElement("button");
+button.textContent = "Switch color";
+document.body.append(button);
+button.style.color = 'black'
+
+function changeColor(item, switcher) {
+  switcher.onclick = () => {
+    if (item.style.color === "red") {
+      item.style.color = "black";
+    } else {
+      item.style.color = "red";
+    }
+  };
+}
+
+changeColor(textBlock, button);
